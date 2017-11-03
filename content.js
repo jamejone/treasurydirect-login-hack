6fef24f8671903a1b832f486e87e11203632159f
James Jones <james.russel.jones@gmail.com>
2017-11-03 09:54:06 -0400

    remove the virtual keyboard too

--- content.js
+++ content.js
@@ -2,4 +2,10 @@ var passwordInputs = document.getElementsByName('password');
 if (passwordInputs.length == 1) {
     passwordInputs[0].readOnly = false;
     passwordInputs[0].className = passwordInputs[0].className.replace(new RegExp('(?:^|\\s)'+ 'pwordinput' + '(?:\\s|$)'), ' ');
+
+    var virtualKeyboard = document.getElementById('virtual_keyboard');
+    if (virtualKeyboard) {
+        virtualKeyboard.outerHTML = "";
+        delete element;
+    }
 }
